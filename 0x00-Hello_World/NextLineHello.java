d2169cd5404ebf6dec8247f986bd8ab11c6fd8d1
KayongoYongo <sknsammie10@gmail.com>
2025-06-15 21:01:58 +0300

    Next line hello

---
+++ 0x00-Hello_World/NextLineHello.java
@@ -0,0 +1,7 @@
+public class NextLineHello {
+	public static void main(String[] args) {
+		// Prints the text without moving to the next line
+		System.out.print("Hello");
+		System.out.print("World");
+	}
+}
