961623ed7b51ad5e2291faba4534349899a906d1
KayongoYongo <sknsammie10@gmail.com>
2025-07-22 21:57:15 +0300

    Clean up

--- 0x00-Hello_World/NextLineHello.java
+++
@@ -1,7 +0,0 @@
-public class NextLineHello {
-	public static void main(String[] args) {
-		// Prints the text without moving to the next line
-		System.out.print("Hello");
-		System.out.print("World");
-	}
-}
--- 0x00-Hello_World/Print2.java
+++
@@ -1,5 +0,0 @@
-public class Print2 {
-    public static void main(String[] args) {
-        System.out.println("Programming is like building a multilingual puzzle\"");
-    }
-}
