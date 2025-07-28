4a7b8ff0fdab3ddd4c30f0b812fe5074b9bf0265
KayongoYongo <sknsammie10@gmail.com>
2025-07-28 16:52:59 +0300

    Use sub strings a s a means of string slicing

---
+++ 0x00-Hello_World/Print8.java
@@ -0,0 +1,11 @@
+public class Print8 {
+	public static void main(String[] args){
+		String word_first_3 = "Quite";
+		String word_last_2 = "Easily";
+		String middle_word = "Doable";
+
+		System.out.println(word_first_3.substring(0,2));
+		System.out.println(word_last_2.substring(4,5));
+		System.out.println(middle_word.substring(2,3));
+	}
+}
