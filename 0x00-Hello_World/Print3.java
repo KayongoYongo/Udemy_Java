1d557bcd0c6cea561c921d4efbaa33d36038714e
KayongoYongo <sknsammie10@gmail.com>
2025-07-28 14:51:52 +0300

    98 Battery street

--- 0x00-Hello_World/Print3.java
+++ 0x00-Hello_World/Print3.java
@@ -1,5 +1,6 @@
 public class Print3{
 	public static void main(String[] args){
+	// To add the gap concatenate the strings in the bracket
 	System.out.print(98 + " ");
 	System.out.print("Battery Street");
 	System.out.println("");
