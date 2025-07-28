d7c1d58546111ad4fc26c72d55af99f974faa700
KayongoYongo <sknsammie10@gmail.com>
2025-07-28 14:50:49 +0300

    98 Battery street

--- 0x00-Hello_World/Print3.java
+++ 0x00-Hello_World/Print3.java
@@ -1,6 +1,6 @@
 public class Print3{
 	public static void main(String[] args){
-	System.out.print(98 );
+	System.out.print(98 + " ");
 	System.out.print("Battery Street");
 	System.out.println("");
 	}
