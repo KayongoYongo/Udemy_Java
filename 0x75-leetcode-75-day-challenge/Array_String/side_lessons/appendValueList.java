b8e9b8a099ea4ca719675b73c9ca53b93be85b40
KayongoYongo <sknsammie10@gmail.com>
2025-08-19 18:59:36 +0300

    Append values to the list

---
+++ 0x75-leetcode-75-day-challenge/Array_String/side_lessons/appendValueList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class appendValueList{
+	public static void main(String[] args){
+		// Declare an empty string list
+		List<Integer> numbers = new ArrayList<>();
+		
+		// Add numbers to the arrays
+		numbers.add(4);
+		numbers.add(56);
+		numbers.add(78);
+
+		System.out.println(numbers);
+	}
+}	
